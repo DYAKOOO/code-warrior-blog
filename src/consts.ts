c08e1115f633877b7bcfebb1ac4c30bd5157f4e9
Dyakooo <diako.rudd@outlook.com>
2024-09-11 15:33:16 +1000

    added the poem

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Neural Divan';
+export const SITE_DESCRIPTION = 'This talks about the most recent breakthough in neural network.';
